d49cc243810511334cc8198f62d973d51e7797fc
jotaalvim <joaoafonsoalvim@gmail.com>
2022-06-07 00:35:55 +0100

    melhor

--- bdf.py
+++ bdf.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import re
+import time
 
 PATH = '/home/jotaalvim/Documents/projetos/chromedriver/chromedriver102.0.5005.61'
 
-driver = webdriver.Chrome(PATH)
+#option = webdriver.ChromeOptions()
+#option.add_argument('headless')
+#driver = webdriver.Chrome( PATH ,options = option)
+driver = webdriver.Chrome( PATH )
 
 driver.get('https://elearning.uminho.pt/')
 
@@ -13,15 +17,14 @@ search = driver.find_element_by_name('user_id')
 search.send_keys('a95191')
 
 search = driver.find_element_by_name('password')
-search.send_keys('garconefeio')
+search.send_keys('113eurosali')
 search.send_keys(Keys.RETURN)
 #login feito
 
-#não funciona tou so a experimentar
-cadeiras = driver.find_element_by_id('div1_1')
-cadeiras2 = driver.findElement(By.xpath("//div[@class='div1_1']/div[@class='div1_1']")).getText();
+#encontrar cadeiras
+time.sleep(0.5)
+sourceCode = str(driver.page_source)
+print(sourceCode)
 
-print(cadeiras.text)
-print(cadeiras2)
-
-
+search3 = re.findall(r'\<li\>(\w+)\<\/li\>',sourceCode)
+print(search3)
